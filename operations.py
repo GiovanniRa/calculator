531ebe9d839c8fded2641ec87b8795c9c69fb3b9
GiovanniRa <GiovanniRa@users.noreply.github.com>
2014-01-24 19:00:10 +0100

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,16 @@
+class Operations(object):
+
+    def addiction(self, a, b):
+        return a + b
+
+    def subtraction(self, a, b):
+        return a - b
+
+    def multiplication(self, a, b):
+        return a * b
+
+    def division(self, a, b):
+        return a / b
+
+    def percentage(self, a, b):
+        return a * b / 100
